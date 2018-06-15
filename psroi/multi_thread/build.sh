bded10de93a1e3a330f0a7c89dad89485f8f0b39
王善昆 <wangshankun2011@hotmail.com>
2018-06-15 11:11:32 +0800

    去掉fast-math 的编译优化，这个优化选项会导致计算库出问题
    #-ffast-math配合O2以上的gcc优化：ceil函数出现问题；
    #在一个接近float32取值精度极限的一个表达式放入ceil中导致向上取整多1
    #这里面是含有变量的表达式，如果是特定的数字组成的计算式放入ceil中则没有问题

--- psroi/multi_thread/build.sh
+++ psroi/multi_thread/build.sh
@@ -5,6 +5,11 @@ gcc -c psroi_pooling.c -w -O3 -o psroi_pooling.o
 gcc parallel.o psroi_pooling.o -lm -lpthread -o  multithreading_psroi
 rm -rf *.o
 
+#-ffast-math配合O2以上的gcc优化：ceil函数出现问题；
+#在一个接近float32取值精度极限的一个表达式放入ceil中导致向上取整多1
+#这里面是含有变量的表达式，如果是特定的数字组成的计算式放入ceil中则没有问题
+#gcc single_psroi.c -w -O3 -ffast-math -lm -o single_psroi
+
 gcc single_psroi.c -w -O3 -lm -o single_psroi
 
 
