20a8e02c049b0fb3f145fc996357abd7e1e87f5a
王善昆 <wangshankun2011@hotmail.com>
2018-11-16 15:26:15 +0800

    nms_wrapper 的cpu only的支持

---
+++ ristretto_rfcn/lib/fast_rcnn/nms_wrapper_cpu_only.py
@@ -0,0 +1,16 @@
+# --------------------------------------------------------
+# Fast R-CNN
+# Copyright (c) 2015 Microsoft
+# Licensed under The MIT License [see LICENSE for details]
+# Written by Ross Girshick
+# --------------------------------------------------------
+
+from fast_rcnn.config import cfg
+from nms.cpu_nms import cpu_nms
+
+def nms(dets, thresh, force_cpu=False):
+    """Dispatch to either CPU or GPU NMS implementations."""
+
+    if dets.shape[0] == 0:
+        return []
+    return cpu_nms(dets, thresh)
