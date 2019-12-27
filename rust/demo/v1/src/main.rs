aa83d64c9cd70982b24be1a01b0fde2b6af5b9ce
朴珞 <wangshankun2011@hotmail.com>
2019-12-27 20:25:22 +0800

    vector 动态结构体数组，调试打印行号，字符串合并，int转字符串

--- rust/demo/v1/src/main.rs
+++ rust/demo/v1/src/main.rs
@@ -1,6 +1,5 @@
 use std::fs::File;
 use std::io::prelude::*;
-
 #[derive(Debug)]
 struct CompressInputImage {
     image_id:      String,
@@ -14,25 +13,33 @@ struct CompressInputImage {
 
 fn main()
 {
-    let mut file = File::open("100.jpg").expect("file not found");
-    let mut buffer = Vec::<u8>::new();
-    let mut buf_len = 0;
-    match  file.read_to_end(&mut buffer)
+    let mut vec_cmp_in :Vec<CompressInputImage> = Vec::new();
+    for idx_img in 100..109
     {
-        //Ok(read_len) => println!("{}", read_len);buf_len = read_len,
-        Ok(read_len) => buf_len = read_len,//赋值给外部的buf_len，而read_len本身生命周期只有这行
-        Err(..) => {}
+        println!("{} {}", file!(), line!()); //行号，文件文件名
+        let img_name    = idx_img.to_string() + ".jpg";//100.jpg  .. 108.jpg
+        let mut file    = File::open(img_name).expect("file not found");
+        let mut buffer  = Vec::<u8>::new();
+        let mut buf_len = 0;
+        match  file.read_to_end(&mut buffer)
+        {
+            Ok(read_len) => buf_len = read_len,//赋值给外部的buf_len，而read_len本身生命周期只有这行
+            Err(..) => println!("file read_to_end error")
+        }
+
+        let cp1 = CompressInputImage { 
+                      image_id:      "1000_9919".to_string() + &idx_img.to_string(),
+                      channel_id:    "hangzhou_0192_1".to_string(),
+                      ts_ms:         1212212 + idx_img,
+                      compress_rate: 16,
+                      image_format:  0,
+                      buf:           buffer,
+                      buf_len:       buf_len as u64, //强转 uszie 到 u64
+                     };
+
+        //println!("cp1 is {:?}", cp1);
+        vec_cmp_in.push(cp1);
+        println!("Vector length: {}", vec_cmp_in.len());
     }
-
-    let cp1 = CompressInputImage { 
-                  image_id:      "1000_9919".to_string(),
-                  channel_id:    "hangzhou_xxx_".to_string(),
-                  ts_ms:         1212212,
-                  compress_rate: 16,
-                  image_format:  0,
-                  buf:           buffer,
-                  buf_len:       buf_len as u64, //强转 uszie 到 u64
-                 };
-
-    println!("cp1 is {:?}", cp1);
+    println!(" {}", vec_cmp_in[3].image_id);
 }
