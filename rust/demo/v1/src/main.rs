0624927fb7358ffbc57e9f99a1c90d9e67ba34d0
朴珞 <wangshankun2011@hotmail.com>
2019-12-27 18:37:58 +0800

    类型强转，错误处理，结构体指针赋值

--- rust/demo/v1/src/main.rs
+++ rust/demo/v1/src/main.rs
@@ -12,27 +12,27 @@ struct CompressInputImage {
     buf_len:       u64,
 }
 
-fn main() {
+fn main()
+{
+    let mut file = File::open("100.jpg").expect("file not found");
+    let mut buffer = Vec::<u8>::new();
+    let mut buf_len = 0;
+    match  file.read_to_end(&mut buffer)
+    {
+        //Ok(read_len) => println!("{}", read_len);buf_len = read_len,
+        Ok(read_len) => buf_len = read_len,//赋值给外部的buf_len，而read_len本身生命周期只有这行
+        Err(..) => {}
+    }
+
     let cp1 = CompressInputImage { 
                   image_id:      "1000_9919".to_string(),
                   channel_id:    "hangzhou_xxx_".to_string(),
                   ts_ms:         1212212,
                   compress_rate: 16,
                   image_format:  0,
-                  buf:           Vec::<u8>::new(),
-                  buf_len:       0,
+                  buf:           buffer,
+                  buf_len:       buf_len as u64, //强转 uszie 到 u64
                  };
 
     println!("cp1 is {:?}", cp1);
-
-    let mut file = File::open("100.jpg").expect("file not found");
-    let mut buffer = Vec::<u8>::new();
-    //let ret   = file.read_to_end(&mut buffer);
-    //println!("{:?}", ret);
-    match  file.read_to_end(&mut buffer)
-    {
-        Ok(n) => println!("{}", n),
-        Err(..) => {}
-    }
-   // println!("{:?}", buffer);
 }
