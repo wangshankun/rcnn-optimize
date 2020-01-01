a223f98f7b8b822e4d4b947b964551a3f0894efc
朴珞 <wangshankun2011@hotmail.com>
2020-01-01 21:22:03 +0800

    sorting struct结构体的排序

---
+++ lab_test/rust_struct_sort/main.rs
@@ -0,0 +1,52 @@
+//https://rust-lang-nursery.github.io/rust-cookbook/algorithms/sorting.html
+#[derive(Debug, Eq, Ord, PartialEq, PartialOrd)]
+struct Person {
+    name: String,
+    age: u32
+}
+
+impl Person {
+    pub fn new(name: String, age: u32) -> Self {
+        Person {
+            name,
+            age
+        }
+    }
+}
+
+fn main() {
+    let mut people = vec![
+        Person::new("Zoe".to_string(), 25),
+        Person::new("Al".to_string(), 60),
+        Person::new("John".to_string(), 1),
+    ];
+
+    // Sort people by derived natural order (Name and age)
+    people.sort();
+
+    assert_eq!(
+        people,
+        vec![
+            Person::new("Al".to_string(), 60),
+            Person::new("John".to_string(), 1),
+            Person::new("Zoe".to_string(), 25),
+        ]);
+
+    // Sort people by age
+    people.sort_by(|a, b| b.age.cmp(&a.age));
+
+    assert_eq!(
+        people,
+        vec![
+            Person::new("Al".to_string(), 60),
+            Person::new("Zoe".to_string(), 25),
+            Person::new("John".to_string(), 1),
+        ]);
+
+    println!("{:?}", people);
+
+    people.sort_by(|a, b| a.age.cmp(&b.age));
+
+    println!("{:?}", people);
+}
+
