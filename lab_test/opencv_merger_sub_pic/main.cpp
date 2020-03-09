94c693c25e73d1a133ed990a042412fc1edb5cd8
wangshankun <wangshankun2011@hotmail.com>
2020-03-09 16:13:47 +0800

    淡入淡出功能

--- lab_test/opencv_merger_sub_pic/main.cpp
+++ lab_test/opencv_merger_sub_pic/main.cpp
@@ -1,6 +1,5 @@
-#include <opencv2/opencv.hpp>
-#include <iostream> 
-#include <fstream> 
+#include<opencv2/opencv.hpp>
+#include<iostream>
 #include <mutex>
 #include <string>
 #include <thread>
@@ -29,7 +28,6 @@ vector<string> split(const string& str, const string& delim)
     return res;
 }
 
-#define SYNOPSIS_OPENMP_THREAD_NUM 4
 
 int main() 
 {
@@ -69,43 +67,43 @@ int main()
         {
             sprintf(fname, "mask_obj_%d.jpg", i);
             Mat mask_obj = imread(fname,0);
+            //cv::threshold(mask_obj, mask_obj, 0, 255, THRESH_BINARY);
             obj_masks.push_back(mask_obj);
         }
     }
     
     Mat bg = imread("bg.jpg");
+    Mat result = bg.clone();
     
     double elapsed;
     struct timespec start, finish;
     clock_gettime(CLOCK_MONOTONIC, &start);
     for (int i =0 ; i < 100; i++)
     {
-        Mat weight_mask(bg.size(), CV_8UC1, Scalar::all(0));
+        Mat weight_mask(result.size(), CV_8UC1, Scalar::all(0));
         {
             for(int i = 0; i < obj_num; i++)
             {
-                Mat mask(bg.size(), CV_8UC1, Scalar::all(0));
-                obj_masks[i] = obj_masks[i] / 255;
-                obj_masks[i].copyTo(mask(obj_boxs[i]));
-                {
-                    weight_mask = weight_mask + mask;
-                }
+                weight_mask(obj_boxs[i]) = weight_mask(obj_boxs[i]) +  obj_masks[i] / 255;
             }
         }
         
-        cv::Mat weight_mask_bgr, whole_mask_bgr;
+        cv::Mat weight_mask_bgr, whole_mask_bgr, whole_mask_bgr_nor;
         cv::cvtColor(weight_mask, weight_mask_bgr, COLOR_GRAY2BGR);
         cv::threshold(weight_mask_bgr, whole_mask_bgr, 0, 255, THRESH_BINARY);
+        cv::bitwise_not(whole_mask_bgr, whole_mask_bgr_nor);
+        whole_mask_bgr_nor.copyTo(result, whole_mask_bgr);
 
         {   
-//            #pragma omp for nowait
             for(int i = 0; i < obj_num; i++)
             {
-                objs[i] = objs[i] / weight_mask_bgr(obj_boxs[i]);
-//                #pragma omp critical
-                {
-                    objs[i].copyTo(bg(obj_boxs[i]), whole_mask_bgr(obj_boxs[i]));
-                }
+                Mat out_obj, obj_with_bg, obj_masks_bgr;
+                addWeighted(objs[i], 0.2, bg(obj_boxs[i]), (1.0 - 0.2), 0, obj_with_bg);
+                cv::cvtColor(obj_masks[i], obj_masks_bgr, COLOR_GRAY2BGR);
+                cv::bitwise_and(obj_with_bg, obj_masks_bgr, out_obj);
+                
+                out_obj = out_obj / weight_mask_bgr(obj_boxs[i]);
+                result(obj_boxs[i]) = result(obj_boxs[i]) +  out_obj;
             }
         }
 
@@ -115,9 +113,9 @@ int main()
     elapsed += (finish.tv_nsec - start.tv_nsec) / 1000000000.0;
     printf("ave elapsed time:%f\r\n",elapsed/100);
 
-    imwrite("result.jpg",bg);
-    //imshow("result", bg);
-    //waitKey();
+    imwrite("result.jpg",result);
+    imshow("result", result);
+    waitKey();
     return 0;
 }
 
