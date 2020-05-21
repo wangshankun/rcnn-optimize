6d13646db24c0e8cf02dfaedcf3260a0d8cd3c35
wangshankun <wangshankun2011@hotmail.com>
2020-05-21 20:41:25 +0800

    cmake编译本地时间做版本信息

---
+++ cmake_compile_local_time_to_version/hello.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+//static char vcs_version_info [] = VCS_VERSION_INFO;
+
+int main() {
+    std::cout << "hello cmake" << std::endl;
+    //std::cout << vcs_version_info  << std::endl;
+    std::cout << VCS_VERSION_INFO  << std::endl;
+    return 0;
+}
